3d42a72228a310e4666a9c73d2f860a836add415
Uwe Bauermann <uwe@bauermann.de>
2024-02-13 18:29:59 +0100

    Mach hinne

--- hello-world.c
+++ hello-world.c
@@ -4,10 +4,9 @@ int main() {
     printf("\n");
     printf("Hello World!\n");
     printf("Hallo Welt!\n");
-    printf("...\n");
+    printf("Hallo Welten!\n");
+    printf("Hallo, dem Rest der Welt!\n");
     printf("...\n");
     printf("\n");
     return 0;
 }
-
-// Dies ist ein Kommentar
